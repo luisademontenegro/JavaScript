d8c31b2f2ad3ed34ddf032d21a2b8f2228c09d38
Luísa de Montenegro <luisademontenegro@hotmail.com>
2022-01-04 20:09:00 -0300

    primeiro commit

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,8 @@
+console.log("Trabalhando com variáveis");
+//JS é case sensitive
+
+const idade = 29;
+const nome = "Luisa";
+
+//não fazer: declarar variável sem o uso do const.
+//mes = "Janeiro";
---
+++ 5-atribuicaoDeVariveis.js
@@ -0,0 +1,17 @@
+console.log("Trabalhando com atribuição de variáveis");
+const idade = 29;
+const primeiroNome = "Luisa";
+//Usando let, a variável pode ser reescrita e/ou sobrescrita sem problemas. Evitar em casos desnecessários
+const sobrenome = "Montenegro";
+
+console.log(primeiroNome + sobrenome);
+//Para separar o nome do sobrenome, usar vírgula ou + " " +
+console.log(primeiroNome + " " + sobrenome);
+
+console.log(`Meu nome é ${primeiroNome}`);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+let contador = 0;
+contador = contador + 1;
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
